37743f8b8135d0d83d2194546beda04204a2b5df
SohamDasIsCool <72563026+SohamDasIsCool@users.noreply.github.com>
2021-05-03 17:42:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,23 @@ function setup() {
   canvas.center();
   video = createCapture(VIDEO);
   video.hide();
+  classifier=ml5.imageClassifier('MobileNet',modelLoaded);
 }
-
-
+function draw(){
+  image(video,0,0,300,300);
+  classifier.classify(video,gotResult);
+}
+function modelLoaded(){
+  console.log("Model Loaded!");
+}
+function gotResult(error,results){
+if(error){
+  console.error(error);
+}
+else{
+  console.log(results);
+  document.getElementById("result_object_name").innerHTML="The object MobileNet sees is "+results[0].label;
+  document.getElementById("result_object_accuracy").innerHTML="MobileNet is accuracte upto "+results[0].confidence.toFixed(3)*100+"%";
+}
+}
 
